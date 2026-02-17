101d4080c18919f5fdd8f4e65bb80aff68471521
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-02-17 14:00:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chaap-cache-v150";
+const CACHE_NAME = "chaap-cache-v151";
 
 const URLS_TO_CACHE = [
   "/",
@@ -75,6 +75,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
